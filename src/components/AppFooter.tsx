ad1e7602949469c0964f6509da6456076d468e37
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-09-09 01:00:56 -0400

    refactor: adds explanation of goal of project and additional info

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,20 +1,23 @@
 import { Typography } from 'antd';
 import { Footer } from 'antd/lib/layout/layout';
+import { LogoutOutlined } from '@ant-design/icons';
 
-const { Text } = Typography;
+const { Link } = Typography;
 
 function AppFooter() {
   return (
     <Footer style={{ backgroundColor: '#051527' }}>
-      <Text
+      <Link
         style={{
           color: 'white',
           display: 'flex',
           justifyContent: 'center',
         }}
+        href="https://github.com/edlopez000/"
       >
-        Footie with link to Github
-      </Text>
+        check out my github
+        <LogoutOutlined rotate={-45} style={{ fontSize: '7px' }} />
+      </Link>
     </Footer>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,18 @@
 import SignInUpButton from '../components/LogInSignUpButton';
+import { Space, Typography } from 'antd';
+
+const { Title, Text } = Typography;
 
 function LoginPage() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', margin: 5 }}>
+    <Space direction="vertical" align="center">
+      <Title style={{ textAlign: 'center' }}>Howdy!</Title>
+      <Text>
+        Welcome to a small practice app where I explore JSON Web Token for
+        protecting API routes and user authentication.
+      </Text>
       <SignInUpButton />
-    </div>
+    </Space>
   );
 }
 
