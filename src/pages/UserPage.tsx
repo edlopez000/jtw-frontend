b0b0ca26f0747c30b7c161c08533b79d6e762dd0
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-09-12 02:52:51 -0400

    feat: adds UserPage

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,16 +1,36 @@
 import { Space, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import { axiosPrivate } from '../api/axiosPrivate';
 
 const { Text, Title } = Typography;
 
+interface User {
+  id: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 function UserPage() {
+  const [user, setUser] = useState<User>();
+
+  const setUserInfo = async () => {
+    const res: any = await axiosPrivate.get('users/profile');
+    setUser(res.data);
+  };
+
+  useEffect(() => {
+    setUserInfo();
+  }, []);
+
   return (
     <div>
       <Typography>
         <Space direction="vertical">
           <Title>Current User Info</Title>
-          <Text>User ID: </Text>
-          <Text>Email: </Text>
-          <Text>Created At: </Text>
+          <Text>User ID: {user && user.id}</Text>
+          <Text>Email: {user && user.email}</Text>
+          <Text>Created At: {user && user.createdAt}</Text>
         </Space>
       </Typography>
     </div>
