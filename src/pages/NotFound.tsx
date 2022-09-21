ab2410397407d5ac0a683cf3be5023277fe4de70
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-09-21 01:54:42 -0400

    graphic: adds Result component from ant.d

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,4 @@
-import { Button, Space, Typography } from 'antd';
+import { Button, Result, Space, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 const { Title } = Typography;
@@ -13,8 +13,12 @@ function NotFound() {
       }}
     >
       <Space direction="vertical" align="center">
-        <Title>Not Found</Title>
-        <Button onClick={() => navigate(-1)}>GO BACK</Button>
+        <Result
+          status="404"
+          title="404"
+          subTitle="How'd you get here? This page doesn't exist."
+          extra={<Button onClick={() => navigate(-1)}>GO BACK</Button>}
+        />
       </Space>
     </div>
   );
