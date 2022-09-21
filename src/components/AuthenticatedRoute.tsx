70ca909e5ac2a47125cafbaf498dc3b64950aa66
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-09-21 01:53:58 -0400

    refactor: meaningful naming

--- src/components/AuthenticatedRoute.tsx
+++ src/components/AuthenticatedRoute.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, redirect, useLocation, useNavigate } from 'react-router-dom';
 import useAuth from '../utils/useAuth';
 
 export const AuthenticatedRoute = ({
@@ -7,12 +7,13 @@ export const AuthenticatedRoute = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const user = useAuth();
+  const { user } = useAuth();
+  const isAuthed = user ? true : false;
   let location = useLocation();
 
   return (
     <div>
-      {user ? (
+      {isAuthed ? (
         children
       ) : (
         <Navigate to="/login" state={{ from: location }} replace />
